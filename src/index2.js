9f1070d5d6caf44132087617d0b715996bea4b6f
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-09 13:43:10 +0900

    Update index2.js

--- src/index2.js
+++ src/index2.js
@@ -6,6 +6,9 @@ console.log(typeof Number(testValue), Number(testValue));
 let testValue = "1"; //string이지만
 console.log(typeof Number(testValue), Number(testValue));  //typeof에서 Number를 명시해줬기 때문에 number로 출력
 
-
+console.log(parseInt(1.2)); //number로 바꿔주는데 1만 남긴다.
+console.log(parseFloat(1.2)); //부동소수점까지 출력
+console.log(Number(1.2)); 
+console.log(Number("1.2")); //따옴표로 문자열로 만들어도 숫자로 출력
 
 
