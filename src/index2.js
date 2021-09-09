c12ca5ae6135bef450fcd7b00ae4f080d8a7944b
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-09 13:37:42 +0900

    js_basic2

---
+++ src/index2.js
@@ -0,0 +1,11 @@
+//데이터 타입 변환
+
+let testValue = 1;
+console.log(typeof Number(testValue), Number(testValue));
+
+let testValue = "1"; //string이지만
+console.log(typeof Number(testValue), Number(testValue));  //typeof에서 Number를 명시해줬기 때문에 number로 출력
+
+
+
+
