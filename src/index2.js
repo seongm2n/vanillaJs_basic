019361557b5825afb042e61b47472f2237675b44
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-09 17:46:58 +0900

    Update index2.js

--- src/index2.js
+++ src/index2.js
@@ -100,4 +100,13 @@ if (age >= 19) {
   console.log("청소년입니다.");
 }
 
+//조건문 else if
+let age = 12;
+if (age >= 19) {
+  console.log("성인입니다");
+} else if (age >= 15) {
+  console.log("청소년입니다.");
+} else {
+  console.log("어린이입니다.");
+}
 
