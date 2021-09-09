ae3bc599f5c99c1eb4d0831ca55acb688c2cea7b
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-09 13:23:03 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,3 +26,11 @@ let testValue = null;
 console.log(typeof testValue, testValue);
 let testValue2;
 console.log(typeof testValue2, testValue2);
+
+let testSymbol1 = Symbol(1);
+let testSymbol2 = Symbol(1);
+console.log(typeof testSymbol1, testSymbol1);
+console.log(typeof testSymbol1, testSymbol2);
+
+console.log(testSymbol1 == testSymbol2);
+console.log(testSymbol1 === testSymbol2);
