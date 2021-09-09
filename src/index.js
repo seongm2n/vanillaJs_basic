f64e49bf04980bd866e3294c2ee51022534dd4a4
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-09 13:12:40 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ testValue = 1;
 console.log(testValue);
 testValue = 2;
 console.log(testValue);
+
+let testValue1 = 1;
+console.log(typeof testValue1);
+const testValue2 = 1.2;
+console.log(typeof testValue2);
+
