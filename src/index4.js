600c5ebf18d176a192c8c6ac16cce53035752238
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-15 12:02:34 +0900

    Update index4.js

--- src/index4.js
+++ src/index4.js
@@ -8,9 +8,15 @@ for (let i =0; i < 5; i++){
 console.log('end');
 
 // 배열과 for문
+
 //length사용 : 배열의 길이만큼 반복
 const data = ['Seongmin', 27, true];
 for(let i = 0; i < data.length; i++){ 
   console.log(data[i]);
 }
 
+//for...of문 
+const data = ['Seongmin', 27, true];
+for(let item of data) {
+  console.log(item);
+}
