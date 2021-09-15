59b520ca43f89afab3d510d7d19894fed4d0a5c3
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-15 19:53:34 +0900

    Update index5.js

--- src/index5.js
+++ src/index5.js
@@ -1,1 +1,24 @@
 //JS + ES6 추가 문법
+
+//1. 삼항연산자
+//condition ? exprIfTrue : exprIfFalse
+
+const data = [1,2];
+
+if(data,length === 0) {
+  console.log("빈 배열입니다.")
+} else {
+  console.log("배열이 차있습니다.")
+}
+//삼항연산자를 써서 위에 if문을 한줄로 표현
+data.length === 0 ? console.log("빈 배열"):console.log("배열이 차있음") //true : false
+
+//2. 함수의 기본 인자(파라미터)값 설정
+
+
+
+
+
+
+
+
