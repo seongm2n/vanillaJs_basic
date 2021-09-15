06bca80b3544177761346fdb81e4a2cbf62d3a0a
seongmin shin <62044613+seongm2n@users.noreply.github.com>
2021-09-15 20:02:42 +0900

    Update index5.js

--- src/index5.js
+++ src/index5.js
@@ -14,12 +14,23 @@ if(data,length === 0) {
 data.length === 0 ? console.log("빈 배열"):console.log("배열이 차있음") //true : false
 
 //2. 함수의 기본 인자(파라미터)값 설정
+//ES6에서 나온 문법으로, 인자 값을 넣지 않을 시, 디폴트로 인자에 넣어지는 값을 설정할 수 있음
 
+//인자 값을 넣지 않을 시의 이슈
 function printData(name) {
   console.log(name);
 }
 printData(); // 값이 정해지지 않아서 undefined로 출력
 
+//디폴트 인자 값 설정
+function printData(item = 1, name = "Seongmin Shin") {
+  console.log(item, name);
+}
+printData();
+
+//3. 구조 분해 할당(비구조화 할당)문법
+//이름만 보면 이해하기 어려운 구조 분해 할당은 배열이나 객체의 속성을 해체해서, 값을 개별 변수에 대입할 수 있게 하는 문법
+//객체와 배열을 좀 더 쉽게 다루는 문법으로 이해하면 됨
 
 
 
